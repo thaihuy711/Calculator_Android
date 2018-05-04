2296bc517e8b837ba7fd36a881c5fb0d24ba6052
thaihuy711 <thaihuy7191112@gmail.com>
2018-05-04 14:17:16 +0700

    Fix bug

--- app/src/main/java/com/thaihuy/calculator/MainActivity.java
+++ app/src/main/java/com/thaihuy/calculator/MainActivity.java
@@ -2,68 +2,38 @@ package com.thaihuy.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnC, btnDoi, btnPhantram, btnPhay, btnCong, btnTru,
-            btnNhan, btnChia, btnBang, btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
-    EditText edKetqua;
+    TextView txtKetqua;
+    Float fThamSo1, fThamSo2;
+    String sToanTu, sXuatManhinh = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addControls();
-        addClicks();
+
+        txtKetqua = findViewById(R.id.txtKetqua);
+        int[] idButton = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9,
+                R.id.btnCong, R.id.btnTru, R.id.btnNhan, R.id.btnChia, R.id.btnBang, R.id.btnPhay, R.id.btnPhantram, R.id.btnDoi, R.id.btnAc};
+        for (int id : idButton) {
+            View view = findViewById(id);
+            view.setOnClickListener(this);
+        }
 
     }
 
-    private void addClicks() {
-        btnC.setOnClickListener(this);
-        btnDoi.setOnClickListener(this);
-        btnPhantram.setOnClickListener(this);
-        btnPhay.setOnClickListener(this);
-        btnCong.setOnClickListener(this);
-        btnTru.setOnClickListener(this);
-        btnNhan.setOnClickListener(this);
-        btnChia.setOnClickListener(this);
-        btnBang.setOnClickListener(this);
-        btn0.setOnClickListener(this);
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-    }
 
-    private void addControls() {
-        edKetqua = findViewById(R.id.edKetqua);
-        btnC = findViewById(R.id.btnAc);
-        btnDoi = findViewById(R.id.btnDoi);
-        btnPhantram = findViewById(R.id.btnPhantram);
-        btnPhay = findViewById(R.id.btnPhay);
-        btnCong = findViewById(R.id.btnCong);
-        btnTru = findViewById(R.id.btnTru);
-        btnNhan = findViewById(R.id.btnNhan);
-        btnChia = findViewById(R.id.btnChia);
-        btnBang = findViewById(R.id.btnBang);
-        btn0 = findViewById(R.id.btn0);
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn5);
-        btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
+    private void ToanTu() {
+        fThamSo1 = Float.parseFloat(txtKetqua.getText().toString());
+        sXuatManhinh = "0";
+        txtKetqua.setText("0");
     }
 
     @Override
@@ -71,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         switch (id) {
             case R.id.btnAc:
+                ToanTu();
                 break;
             case R.id.btnDoi:
                 break;
@@ -79,35 +50,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnPhay:
                 break;
             case R.id.btnCong:
+                sToanTu = "+";
+                ToanTu();
                 break;
             case R.id.btnTru:
+                sToanTu = "-";
+                ToanTu();
                 break;
             case R.id.btnNhan:
+                sToanTu = "*";
+                ToanTu();
                 break;
             case R.id.btnChia:
+                sToanTu = "/";
+                ToanTu();
                 break;
             case R.id.btnBang:
+                Float kq = null;
+                fThamSo2 = Float.parseFloat(txtKetqua.getText().toString());
+                if (sToanTu == "+") {
+                    kq = fThamSo1 + fThamSo2;
+                }
+                if (sToanTu == "-") {
+                    kq = fThamSo1 - fThamSo2;
+                }
+                if (sToanTu == "*") {
+                    kq = fThamSo1 * fThamSo2;
+                }
+                if (sToanTu == "/") {
+                    kq = fThamSo1 / fThamSo2;
+                }
+                txtKetqua.setText(String.valueOf(kq));
+                fThamSo1 = 0.0f;
+                fThamSo2 = 0.0f;
+                sXuatManhinh = "0";
+
+
                 break;
-            case R.id.btn0:
-                break;
-            case R.id.btn1:
-                break;
-            case R.id.btn2:
-                break;
-            case R.id.btn3:
-                break;
-            case R.id.btn4:
-                break;
-            case R.id.btn5:
-                break;
-            case R.id.btn6:
-                break;
-            case R.id.btn7:
-                break;
-            case R.id.btn8:
-                break;
-            case R.id.btn9:
-                break;
+            default:
+                if (sXuatManhinh.equals("0")) {
+                    sXuatManhinh = "";
+                }
+                sXuatManhinh += ((Button) view).getText().toString();
+                txtKetqua.setText(sXuatManhinh);
 
         }
     }
